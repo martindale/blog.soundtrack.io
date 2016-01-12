f22a21394f279c23585e962ac0e72ed59a623af3
Eric Martindale <eric@ericmartindale.com>
2016-01-12 17:58:44 -0500

    Re-add config.

--- config/index.js
+++ config/index.js
@@ -5,6 +5,15 @@ module.exports = {
     mission: 'thoughts from the soundtrack.io team.',
     icon: 'music'
   },
+  config: {
+    allowPublicRegistration: false, // let other people register
+    characterLimit: false // set to a number, such as 140.
+  },
+  users: { // users will be created on startup, password logged to console
+    'admin': { // admin username will be 'admin', change if you'd like
+      roles: ['admin'] // what roles this user will have
+    }
+  },
   database: {
     name: 'soundtrack'
   },
