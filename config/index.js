9a8ee95604f5593457b5d89848abaaf7cd22ad51
Eric Martindale <eric@ericmartindale.com>
2016-01-12 17:57:43 -0500

    Revert "Package up and simplify initial setup."
    This reverts commit 35d27d6c7fd310c0b20f6977abbed6940edc8210.

--- config/index.js
+++ config/index.js
@@ -1,25 +1,16 @@
 module.exports = {
   service: {
-    name: 'Your Stream Name', // change this!
-    synopsis: 'Thoughts and musings from me.', // and this...
-    mission: 'Self-hosted writing and content sharing, powered by Maki', // also
-    icon: 'write' // pick from: http://semantic-ui.com/elements/icon.html
-  },
-  config: {
-    allowPublicRegistration: false, // let other people register
-    characterLimit: false // set to a number, such as 140.
-  },
-  users: { // users will be created on startup, password logged to console
-    'admin': { // admin username will be 'admin', change if you'd like
-      roles: ['admin'] // what roles this user will have
-    }
+    name: 'melodies',
+    synopsis: 'thoughts from soundtrack.io',
+    mission: 'thoughts from the soundtrack.io team.',
+    icon: 'music'
   },
   database: {
-    name: 'melody' // only change if you know what you're doing
+    name: 'soundtrack'
   },
   services: {
     http: {
-      port: 3000 // you might want this to be some other port, like 80
+      port: 13005
     }
   }
 };
